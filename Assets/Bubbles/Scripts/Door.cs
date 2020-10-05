44e5ad8f56a1e82d395e629e923ff23e387a21d7
Matt <matt@hookman.co.uk>
2020-10-05 22:02:26 +0100

    Sounds added

--- Assets/Bubbles/Scripts/Door.cs
+++ Assets/Bubbles/Scripts/Door.cs
@@ -10,6 +10,7 @@ namespace BubbleDistortionPhysics
         public GameObject DoorPanel;
         public GameObject KeyLock;
         private int _intTimeToOpen = 1;
+        private bool _doorOpened;
 
         public void Start()
         {            
@@ -17,17 +18,31 @@ namespace BubbleDistortionPhysics
         
         public IEnumerator OpenDoor()
         {
-            var currentPos = DoorPanel.transform.localPosition;
-            var currentScale = DoorPanel.transform.localScale;
-            var rigidBody = DoorPanel.GetComponent<Rigidbody>();
-            var t = 0f;
-            while (t < 1)
+            if (!_doorOpened)
             {
-                t += Time.deltaTime / _intTimeToOpen;
-                DoorPanel.transform.localPosition = Vector3.Lerp(currentPos, new Vector3(currentPos.x, currentPos.y + 1, currentPos.z), t);
-                DoorPanel.transform.localScale = Vector3.Lerp(currentScale, new Vector3(currentScale.x, currentScale.y - 1.99f, currentScale.z), t);
-                yield return null;
-            }
+                _doorOpened = true;
+                KeyLock.GetComponent<AudioSource>().Play();
+
+                var t = 0f;
+                while (t < 1)
+                {
+                    t += Time.deltaTime;
+                    yield return null;
+                }
+
+                DoorPanel.GetComponent<AudioSource>().Play();
+                var currentPos = DoorPanel.transform.localPosition;
+                var currentScale = DoorPanel.transform.localScale;
+                var rigidBody = DoorPanel.GetComponent<Rigidbody>();
+                t = 0;
+                while (t < 2)
+                {
+                    t += Time.deltaTime / _intTimeToOpen;
+                    DoorPanel.transform.localPosition = Vector3.Lerp(currentPos, new Vector3(currentPos.x, currentPos.y + 1, currentPos.z), t);
+                    DoorPanel.transform.localScale = Vector3.Lerp(currentScale, new Vector3(currentScale.x, currentScale.y - 1.99f, currentScale.z), t);
+                    yield return null;
+                }
+            }            
         }        
     }
 }
